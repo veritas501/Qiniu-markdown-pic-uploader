8d1b5726e845d514b56e31dea395e534a8c8c361
veritas501 <xqt526@outlook.com>
2017-12-04 19:24:15 +0800

    bug fix

--- qiniu_upload_csharp/qiniu_upload_csharp/Form1.Designer.cs
+++ qiniu_upload_csharp/qiniu_upload_csharp/Form1.Designer.cs
@@ -155,7 +155,7 @@
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form1";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Qiniu Pic Markdown Uploader ver0.1";
+			this.Text = "Qiniu Pic Markdown Uploader ver0.11";
 			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
